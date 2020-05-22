b3e26d31b5c78712b180a11237ad5f4e1e9b48d2
pavlentygood <pavlentygood@yandex.ru>
2020-05-22 23:31:07 +0300

    Updates

---
+++ precache-manifest.b3d15235b8198fec5c1615ceda0cb244.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2b206edf77896367cf1c84af61577a0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b27c0572c4f52a0563ca",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "6980802924541fdf6528",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "b27c0572c4f52a0563ca",
+    "url": "./static/js/2.9c1e4ac8.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.9c1e4ac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6980802924541fdf6528",
+    "url": "./static/js/main.abf539db.chunk.js"
+  },
+  {
+    "revision": "122e2e6a91158f3d98e8",
+    "url": "./static/js/runtime-main.fccab29b.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
